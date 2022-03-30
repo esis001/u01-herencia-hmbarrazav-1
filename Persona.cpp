8a4fadf6b7328664b920081d953469b060382d39
hmbarrazav <humbarraza@upt.pe>
2022-03-30 11:40:35 -0500

    Implementación de la clase Docente

--- Persona.cpp
+++ Persona.cpp
@@ -17,6 +17,28 @@ Persona :: Persona(string n, string d, int e){
     edad = e;
 }
 
+class Docente : public Persona{
+    private:
+        int sueldo;
+        string especialidad;
+    public:
+        Docente(string, string, int, int, string);
+        void mostrarDocente();
+        //void ingresarNotas();
+        //void visualizarRegistro();
+};
+
+Docente :: Docente(string n, string d, int e, int s, string es) : Persona(n,d,e){
+    sueldo = s;
+    especialidad = es;       
+}
+
+void Docente :: mostrarDocente(){
+    mostrarDatos();
+    cout<<"\t tiene un sueldo de S/"<<sueldo<<" y tiene la especialidad: ";
+    cout<<especialidad;
+}
+
 void Persona :: mostrarDatos(){
     cout<<nombres<<" tiene el DNI# "<<DNI<<" y tiene "<<edad;
     cout<<" anios de edad."<<endl;
@@ -25,5 +47,7 @@ void Persona :: mostrarDatos(){
 int main(){
     Persona p("Juan Perez", "40302010",25);
     p.mostrarDatos();
+    Docente d("Pablo Iglesias", "10203040", 54, 3000, "Auditoria informatica");
+    d.mostrarDocente();
     return 0;
 }
